c779d06d29cfa1c5988171b40022713c818df2bb
Maksym Nikityuk <maksym71417@gmail.com>
2023-08-18 17:53:34 +0300

    quiz app logic implemented

---
+++ pyquizapp.py
@@ -0,0 +1,73 @@
+class QuizApp:
+
+    def __init__(self) -> None:
+        self.username = ""
+
+    def startup(self):
+        self.greeting()
+
+        self.username = input("Please enter your username: ")
+        print(f"Welcome, {self.username}")
+
+    def greeting(self):
+        print("~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-")
+        print("````````Welcome to PYQUIZ```````")
+        print("~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-")
+        print()
+
+    def menu_header(self):
+        print("--------------------------------")
+        print("Please make a selection:")
+        print("(M): Repeat this menu")
+        print("(L): List quizzes")
+        print("(T): Take a quiz")
+        print("(E): Exit program")
+
+    def menu_error(self):
+        print("That's not a valid selection. Please try again.")
+
+    def goodbye(self):
+        print("-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~")
+        print(f"Thanks for using PyQuiz, {self.username}!")
+        print("-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~")
+
+    def menu(self):
+        self.menu_header()
+
+        selection = ""
+
+        while True:
+            selection = input("Selection? ")
+            if len(selection) == 0:
+                self.menu_error()
+                continue
+
+            selection = selection.capitalize()
+
+            if selection[0] == "E":
+                self.goodbye()
+                break
+            elif selection[0] == "M":
+                self.menu_header()
+            elif selection[0] == "L":
+                print("\nAvailable quizzes are: ")
+                print("-----------------------\n")
+                continue
+            elif selection[0] == "T":
+                 try:
+                     quiznum = int(input("Enter quiz number please--> "))
+                     print(f"You have selected quiz number {quiznum}")
+                 except:
+                     self.menu_error()
+
+            else:
+                self.menu_error()
+                continue
+    def run(self):
+        self.startup()
+        self.menu()
+
+
+if __name__ == "__main__":
+    app = QuizApp()
+    app.run()
